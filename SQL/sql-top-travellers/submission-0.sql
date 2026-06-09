3f66d83ee17c825b1fa7027d6710567cbcb3f3e8
tar7nic <154546879+tar7nic@users.noreply.github.com>
2026-06-09 21:42:35 +0530

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,5 @@
+SELECT u.name, COALESCE(SUM(r.distance),0) as travelled_distance
+FROM users u
+LEFT JOIN rides r ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC
